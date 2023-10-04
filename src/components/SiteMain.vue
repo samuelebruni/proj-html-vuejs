f4409bb4f187d9f25eb1a66fdcb28fa5c22a6c14
samuelebruni <samuele_bruni@hotmail.com>
2023-10-04 12:51:16 +0200

    end tanttobuy section in SiteMain.vue

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -122,8 +122,8 @@ export default {
 
 
             <div class="row">
-                <div class="col-2">
-                    <div class="card d-flex flex-column justify-content-center shadow-sm border-0">
+                <div class="col-2 shadow-sm">
+                    <div class="card d-flex flex-column justify-content-center border-0">
                         <img src="../assets/img/cabrio.jpg" alt="" class="pt-3">
                         <div class="text-center d-flex flex-column pt-3">
                             <span>Cabrio</span>
@@ -131,8 +131,8 @@ export default {
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
-                    <div class="card d-flex flex-column justify-content-center shadow-sm border-0">
+                <div class="col-2 shadow-sm">
+                    <div class="card d-flex flex-column justify-content-center border-0">
                         <img src="../assets/img/coupe.jpg" alt="" class="pt-3">
                         <div class="text-center d-flex flex-column pt-3">
                             <span>Coupe</span>
@@ -140,8 +140,8 @@ export default {
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
-                    <div class="card d-flex flex-column justify-content-center shadow-sm border-0">
+                <div class="col-2 shadow-sm">
+                    <div class="card d-flex flex-column justify-content-center border-0">
                         <img src="../assets/img/hatchback.jpg" alt="" class="pt-3">
                         <div class="text-center d-flex flex-column pt-3">
                             <span>Hatchback</span>
@@ -149,8 +149,8 @@ export default {
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
-                    <div class="card d-flex flex-column justify-content-center shadow-sm border-0">
+                <div class="col-2 shadow-sm">
+                    <div class="card d-flex flex-column justify-content-center border-0">
                         <img src="../assets/img/pickup.jpg" alt="" class="pt-3">
                         <div class="text-center d-flex flex-column pt-3">
                             <span>Pick Up</span>
@@ -158,8 +158,8 @@ export default {
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
-                    <div class="card d-flex flex-column justify-content-center shadow-sm border-0">
+                <div class="col-2 shadow-sm">
+                    <div class="card d-flex flex-column justify-content-center border-0">
                         <img src="../assets/img/sedan.jpg" alt="" class="pt-3">
                         <div class="text-center d-flex flex-column pt-3">
                             <span>Sedan</span>
@@ -167,8 +167,8 @@ export default {
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
-                    <div class="card d-flex flex-column justify-content-center shadow-sm border-0">
+                <div class="col-2 shadow-sm">
+                    <div class="card d-flex flex-column justify-content-center border-0">
                         <img src="../assets/img/suv.jpg" alt="" class="pt-3">
                         <div class="text-center d-flex flex-column pt-3">
                             <span>SUV</span>
@@ -575,11 +575,98 @@ export default {
                 </div>
             </div>
 
+            <div>
+                <div class="d-flex justify-content-center py-3">
+                    <span class="btn btn-dark text-white mx-5 fw-bold rounded-0 px-4 py-3">
+                        <span>Show All Cars </span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                        </svg></span>
+                </div>
+            </div>
         </div>
-
 
     </section>
     <!--ENDSEARCH-->
+
+    <!--WANT TO BUY SECTION-->
+    <section id="wanttobuy" class="mb-5">
+        <div class="container mb-5">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-6 backimg">
+                    <div class="position-relative">
+                        <img src="../assets/img/aoutcar-about-1.png" alt="">
+                        <div
+                            class="text-white bg-black circle text-center d-flex flex-column justify-content-center position-absolute mypositionfirst">
+                            <div class="fw-bold myfont">2 4</div>
+                            <h4>Years of Experience</h4>
+                        </div>
+                        <div
+                            class="text-white bg-black circle text-center d-flex flex-column justify-content-center position-absolute mypositionsecond">
+                            <div class="fw-bold myfont">2 4 0</div>
+                            <h4>Special Expert Team</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-4">
+                    <div>
+                        <h3 class="mt-5">Want to buy or sell a veichle?</h3>
+                        <p>
+                            You can put yet vehicles for sale by registering on our website. Whether you are a dealer or
+                            sell
+                            personally. Sell your vehicle in the most profitable way. With this system, wich has millions of
+                            members, you will be able to bu and sell vehicles quickly.
+                        </p>
+                        <img src="../assets/img/divider.jpg" alt="">
+                        <div class="d-flex align-items-center gap-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor"
+                                class="bi bi-car-front" viewBox="0 0 16 16">
+                                <path
+                                    d="M4 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm10 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2H6ZM4.862 4.276 3.906 6.19a.51.51 0 0 0 .497.731c.91-.073 2.35-.17 3.597-.17 1.247 0 2.688.097 3.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 10.691 4H5.309a.5.5 0 0 0-.447.276Z" />
+                                <path
+                                    d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679c.033.161.049.325.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.807.807 0 0 0 .381-.404l.792-1.848ZM4.82 3a1.5 1.5 0 0 0-1.379.91l-.792 1.847a1.8 1.8 0 0 1-.853.904.807.807 0 0 0-.43.564L1.03 8.904a1.5 1.5 0 0 0-.03.294v.413c0 .796.62 1.448 1.408 1.484 1.555.07 3.786.155 5.592.155 1.806 0 4.037-.084 5.592-.155A1.479 1.479 0 0 0 15 9.611v-.413c0-.099-.01-.197-.03-.294l-.335-1.68a.807.807 0 0 0-.43-.563 1.807 1.807 0 0 1-.853-.904l-.792-1.848A1.5 1.5 0 0 0 11.18 3H4.82Z" />
+                            </svg>
+                            <div>
+                                <h5 class="pt-3">Model Vehicles</h5>
+                                <p>We are sure thet you will find vehicles of suitable models on our website.</p>
+                            </div>
+
+                        </div>
+                        <div class="d-flex align-items-center gap-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor"
+                                class="bi bi-car-front" viewBox="0 0 16 16">
+                                <path
+                                    d="M4 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm10 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2H6ZM4.862 4.276 3.906 6.19a.51.51 0 0 0 .497.731c.91-.073 2.35-.17 3.597-.17 1.247 0 2.688.097 3.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 10.691 4H5.309a.5.5 0 0 0-.447.276Z" />
+                                <path
+                                    d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679c.033.161.049.325.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.807.807 0 0 0 .381-.404l.792-1.848ZM4.82 3a1.5 1.5 0 0 0-1.379.91l-.792 1.847a1.8 1.8 0 0 1-.853.904.807.807 0 0 0-.43.564L1.03 8.904a1.5 1.5 0 0 0-.03.294v.413c0 .796.62 1.448 1.408 1.484 1.555.07 3.786.155 5.592.155 1.806 0 4.037-.084 5.592-.155A1.479 1.479 0 0 0 15 9.611v-.413c0-.099-.01-.197-.03-.294l-.335-1.68a.807.807 0 0 0-.43-.563 1.807 1.807 0 0 1-.853-.904l-.792-1.848A1.5 1.5 0 0 0 11.18 3H4.82Z" />
+                            </svg>
+                            <div>
+                                <h5 class="pt-3">Model Vehicles</h5>
+                                <p>We are sure thet you will find vehicles of suitable models on our website.</p>
+                            </div>
+
+                        </div>
+                        <div>
+                            <div class="py-3">
+                                <span class="btn btn-dark text-white fw-bold rounded-0 px-4 py-3">
+                                    <span>Get to know us </span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd"
+                                            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                    </svg></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--ENDWANTTOBUY-->
 </template>
 <style scoped>
 #site_jumbotron {
@@ -588,5 +675,10 @@ export default {
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
+}
+
+.backimg {
+    background-image: url(../assets/img/circle-auto-car-1.png);
+    height: 600px;
 }
 </style>
