754f7d7f20537b507fb3fce6247114c43010f8c6
samuelebruni <samuele_bruni@hotmail.com>
2023-10-04 14:12:49 +0200

    end customers section in SiteMain.vue

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -576,7 +576,7 @@ export default {
             </div>
 
             <div>
-                <div class="d-flex justify-content-center py-3">
+                <div class="d-flex justify-content-center py-3 my-4">
                     <span class="btn btn-dark text-white mx-5 fw-bold rounded-0 px-4 py-3">
                         <span>Show All Cars </span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -592,8 +592,8 @@ export default {
     <!--ENDSEARCH-->
 
     <!--WANT TO BUY SECTION-->
-    <section id="wanttobuy" class="mb-5">
-        <div class="container mb-5">
+    <section id="wanttobuy" class="mb-5 pb-5">
+        <div class="container mb-5 pb-5">
             <div class="row">
                 <div class="col-1"></div>
                 <div class="col-6 backimg">
@@ -667,6 +667,117 @@ export default {
         </div>
     </section>
     <!--ENDWANTTOBUY-->
+    <!--CUSTOMER-->
+    <section id="customer" class="mt-5 pt-5">
+        <div class="container">
+            <div class="d-flex flex-column justify-content-center align-items-center text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="40" fill="currentColor"
+                    class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
+                    <path
+                        d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
+                </svg>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                    </svg>
+                </div>
+            </div>
+            <div class="text-center text-white mt-5">
+                <h3 class="pb-3">What Are Our Customers Saying ?</h3>
+                <p>Opinions from our happy customers.</p>
+            </div>
+            <div>
+                <div class="row">
+                    <div class="col-3 mt-5">
+                        <div class="card p-5 d-flex flex-column align-items-center position-relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                                class="position-absolute myposition bi bi-quote" viewBox="0 0 16 16">
+                                <path
+                                    d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
+                            </svg>
+                            <p class="text-center position-relative">We are a gallery vehicle dealer. Before we met this
+                                site, our sales, were
+                                sluggish.</p>
+                            <div class="py-4">
+                                <img src="../assets/img/man1.png" style="width: 120px;">
+                            </div>
+                            <p>Adam</p>
+
+                        </div>
+                    </div>
+                    <div class="col-3 mt-5">
+                        <div class="card p-5 d-flex flex-column align-items-center position-relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                                class="position-absolute myposition bi bi-quote" viewBox="0 0 16 16">
+                                <path
+                                    d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
+                            </svg>
+                            <p class="text-center position-relative">We are a gallery vehicle dealer. Before we met this
+                                site, our sales, were
+                                sluggish.</p>
+                            <div class="py-4">
+                                <img src="../assets/img/testimonial2-1.png" style="width: 120px;">
+                            </div>
+                            <p>Cristian</p>
+
+                        </div>
+                    </div>
+                    <div class="col-3 mt-5">
+                        <div class="card p-5 d-flex flex-column align-items-center position-relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                                class="position-absolute myposition bi bi-quote" viewBox="0 0 16 16">
+                                <path
+                                    d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
+                            </svg>
+                            <p class="text-center position-relative">We are a gallery vehicle dealer. Before we met this
+                                site, our sales, were
+                                sluggish.</p>
+                            <div class="py-4">
+                                <img src="../assets/img/man2.png" style="width: 120px;">
+                            </div>
+                            <p>Robert</p>
+
+                        </div>
+                    </div>
+                    <div class="col-3 mt-5">
+                        <div class="card p-5 d-flex flex-column align-items-center position-relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                                class="position-absolute myposition bi bi-quote" viewBox="0 0 16 16">
+                                <path
+                                    d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
+                            </svg>
+                            <p class="text-center position-relative">We are a gallery vehicle dealer. Before we met this
+                                site, our sales, were
+                                sluggish.</p>
+                            <div class="py-4">
+                                <img src="../assets/img/testimonial4-1.png" style="width: 120px;">
+                            </div>
+                            <p>Elizabeth</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center gap-2 mt-4">
+                <div class="mywidthfirst mt-5 pt-2 d-flex justify-content-center"></div>
+                <div class="mywidthsecond mt-5 pt-2 d-flex justify-content-center"></div>
+            </div>
+
+        </div>
+    </section>
+    <!--ENDCUSTOMER-->
 </template>
 <style scoped>
 #site_jumbotron {
@@ -680,5 +791,10 @@ export default {
 .backimg {
     background-image: url(../assets/img/circle-auto-car-1.png);
     height: 600px;
+}
+
+#customer {
+    background-image: url(../assets/img/comment-bg-autocar.jpg);
+    height: 850px;
 }
 </style>
